6556d4a8545e93d3bf7b4e1d25075d821560d171
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-02-24 13:55:13 +0100

    Paritally added a cpr validation method

--- OopLearning.BL/Person.cs
+++ OopLearning.BL/Person.cs
@@ -26,7 +26,7 @@ namespace OopLearning.BL
                 (bool isValid, string errorMessage) validationResult = ValidateName(value);
                 if(!validationResult.isValid)
                 {
-                    throw new ArgumentOutOfRangeException(nameof(name), validationResult.errorMessage);
+                    throw new ArgumentException(nameof(name), validationResult.errorMessage);
                 }
                 if(value != name)
                 {
@@ -35,7 +35,22 @@ namespace OopLearning.BL
             }
         }
         public DateTime BirthDate { get; }
-        public string Cpr { get; set; }
+        public string Cpr
+        {
+            get { return cpr; }
+            set
+            {
+                (bool isValid, string errorMessage) validationResult = ValidateCpr(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentException(nameof(cpr), validationResult.errorMessage);
+                }
+                if(value != cpr)
+                {
+                    cpr = value;
+                }
+            }
+        }
         public bool IsWoman { get; set; }
 
         public static (bool, string) ValidateName(string name)
@@ -44,17 +59,33 @@ namespace OopLearning.BL
             {
                 return (false, "Navnet er null!");
             }
-            if(name.Length < 2)
-            {
-                return (false, "Navnet skal være større end 1!");
-            }
+
             if(string.IsNullOrWhiteSpace(name))
             {
                 return (false, "Navnet indeholder kun whitespaces!");
             }
+            if(name.Length < 2)
+            {
+                return (false, "Navnet skal være større end 1!");
+            }
             if(!name.Contains(" "))
             {
                 return (false, "Navnet skal indeholde mindst 1 mellemrum!");
+            }
+            else
+            {
+                return (true, "");
+            }
+        }
+        public static (bool, string) ValidateCpr(string cpr)
+        {
+            if(cpr is null)
+            {
+                return (false, "CPR er null!");
+            }
+            if(cpr.Length != 10)
+            {
+                return (false, "CPR må kun være på 10 cifre!");
             }
             else
             {
--- OopLearning.BL/Program.cs
+++ OopLearning.BL/Program.cs
@@ -6,7 +6,22 @@ namespace OopLearning.BL
     {
         static void Main()
         {
-           
+            string nameInput = "Ib";
+            string cprInput = "";
+            (bool isValid, string errorMessage) nameValidationResult = Person.ValidateName(nameInput);
+            (bool isValid, string errorMessage) cprValidationResult = Person.ValidateCpr(cprInput);
+            if(!nameValidationResult.isValid)
+            {
+                Console.WriteLine(nameValidationResult.errorMessage);
+            }
+            if(!cprValidationResult.isValid)
+            {
+                Console.WriteLine(cprValidationResult.errorMessage);
+            }
+            else
+            {
+                Person person = new Person(nameInput, new DateTime(0001, 01, 01), cprInput, false);
+            }
         }
     }
 }
