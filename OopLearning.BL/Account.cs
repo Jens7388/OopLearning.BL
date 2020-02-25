8792cd237035299f88c5e346ec3252c2b9d5a2a4
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-02-25 10:29:49 +0100

    Added a validation method for department number

--- OopLearning.BL/Account.cs
+++ OopLearning.BL/Account.cs
@@ -21,8 +21,6 @@ namespace OopLearning.BL
             set
             {
                 (bool isValid, string errorMessage) validationResult = ValidateAccountNumber(value);
-                if(!validationResult.isValid)
-                {
                     if(!validationResult.isValid)
                     {
                         throw new ArgumentException(nameof(accountNumber), validationResult.errorMessage);
@@ -31,7 +29,24 @@ namespace OopLearning.BL
                     {
                         accountNumber = value;
                     }
+                
+            }
+        }
+        public string DepartmentNumber
+        {
+            get { return departmentNumber; }
+            set
+            {
+                (bool isValid, string errorMessage) validationResult = ValidateDepartmentNumber(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentException(nameof(departmentNumber), validationResult.errorMessage);
                 }
+                if(value != departmentNumber)
+                {
+                    departmentNumber = value;
+                }
+                
             }
         }
         public static (bool, string) ValidateAccountNumber(string accountNumber)
@@ -43,6 +58,25 @@ namespace OopLearning.BL
             if(accountNumber.Length != 10)
             {
                 return (false, "AccountNumber må kun være på 10 cifre!");
+            }
+            else
+            {
+                return (true, String.Empty);
+            }
+        }
+        public static (bool, string) ValidateDepartmentNumber(string departmentNumber)
+        {
+            if(departmentNumber is null)
+            {
+                return (false, "DepartmentNumber er null");
+            }
+            if(departmentNumber.Length != 4)
+            {
+                return (false, "DepartmentNumber må kun være på 4 cifre!");
+            }
+            if(departmentNumber.Substring(0, 1) != "0")
+            {
+                return (false, "DepartmentNumber skal starte med et 0!");
             }
             else
             {
